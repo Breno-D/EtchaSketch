69307a620574260692527230959fa81ef7ce8f37
Breno Dória <brenord@hotmail.com.br>
2024-01-31 19:53:24 -0300

    [ADD] Reset And Resize

--- javascript.js
+++ javascript.js
@@ -1,5 +1,15 @@
 const container = document.querySelector('.container');
-console.log(container);
+const body = document.querySelector('body');
+
+const stylesheet = document.styleSheets[0];
+let blockRule;
+
+for(let i = 0; i < stylesheet.cssRules.length; i++) {
+    if(stylesheet.cssRules[i].selectorText === '.block') {
+        blockRule = stylesheet.cssRules[i];
+    }
+  }
+
 
 for(let i = 0 ; i<10000; i++)
 {
@@ -20,12 +30,36 @@ divs.forEach((div) =>
     });
 
 const resetButton = document.querySelector('button');
-resetButton.addEventListener('click', removeColor);
+resetButton.addEventListener('click', startResetProcess);
 
-function removeColor()
+function startResetProcess()
 {
-    divs.forEach((div) => { 
+    let divsToBeRemoved = document.querySelectorAll('.container > div');
+    divsToBeRemoved.forEach((div) => { 
         div.classList.remove("coloredBlock");
+        div.classList.add("disabled");
+        container.removeChild(div);
+        body.appendChild(div);
     })
-
+    let newNumberOfSquaresPerSide = window.prompt("How many squares per side in the new canvas? (valueXvalue) max 100", 100);
+    setNewCanvas(newNumberOfSquaresPerSide);
 }
+
+function setNewCanvas(squaresPerSide)
+{
+    let maxNumberOfDivs = squaresPerSide*squaresPerSide;
+    let counter = 0;
+    divs.forEach((div) =>{
+        if(counter < maxNumberOfDivs)
+        {
+            div.classList.remove("disabled");
+            body.removeChild(div);
+            container.appendChild(div);
+            counter++;
+        }
+        let squareSideValue = 900/squaresPerSide;
+        blockRule.style.setProperty('width', squareSideValue+"px");
+        blockRule.style.setProperty('height', squareSideValue+"px");
+    })
+}
+
